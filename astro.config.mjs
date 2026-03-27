3a4ad4486d73adec512742eeec6e8fa10cc387b1
user <user@mail.com>
2026-03-27 23:24:45 +0100

    feat: add sitemap integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://alexdevflow.github.io',
   base: '/',
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => !page.includes('/fiorista/'),
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
